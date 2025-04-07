4e7968e98c16575c07cef414592ca8c9a1f2e751
Malaydewangan09 <malaydewangan310@gmail.com>
2025-04-07 18:06:03 +0530

    fix(influxdb): fix docker compose dir and setup file

--- .github/setup-unit.sh
+++ .github/setup-unit.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# Navigate to the root directory
 cd "$(dirname "$0")/.."
-docker compose -f docker-compose-ci.yml up -d
-sleep 10
-curl -s -o .github/workflows/air-sensor-data.lp https://raw.githubusercontent.com/influxdata/influxdb2-sample-data/master/air-sensor-data/air-sensor-data.lp
-docker compose -f docker-compose-ci.yml exec influxdb sh -c ' influx write --bucket test-bucket --org my-org --token my-token --precision ns --file /docker-entrypoint-initdb.d/air-sensor-data.lp'
 
+# Start the Docker containers in the background
+docker compose -f docker-compose-ci.yml up -d
+sleep 10  # Wait for containers to initialize
+
+# Fetch the air sensor data
+curl -s -o .github/workflows/air-sensor-data.lp https://raw.githubusercontent.com/influxdata/influxdb2-sample-data/master/air-sensor-data/air-sensor-data.lp
+
+# Write the fetched data to InfluxDB
+docker compose -f docker-compose-ci.yml exec influxdb sh -c 'influx write --bucket test-bucket --precision ns --file /docker-entrypoint-initdb.d/air-sensor-data.lp'
+
+# Query data to verify it was written
+result=$(docker compose -f docker-compose-ci.yml exec influxdb sh -c 'influx query "from(bucket: \"test-bucket\") |> range(start: -1h) |> count()"')
+
+# Check if the query result is greater than 0, indicating data was written
+if [[ $result =~ "count" && ${result#*:} -gt 0 ]]; then
+  echo "Data was successfully written to InfluxDB!"
+else
+  echo "No data found in InfluxDB. There may have been an issue with writing the data."
+fi
